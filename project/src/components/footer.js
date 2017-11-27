4282f8c6aa320d4c41540c1e4216ea5322f838c1
yujing-0504 <593224885@qq.com>
2017-11-27 13:02:28 +0800

    footer样式

--- project/src/components/footer.js
+++ project/src/components/footer.js
@@ -13,7 +13,39 @@ class Footer extends Component{
   render(){
     return(
         <div className="footer">
-         <Link to='./ShoppingCart'>sfsd</Link>
+        <ul>
+           <li>
+          <Link to='./ShoppingCart'>
+            <i className="iconfont">&#xe62b;</i><br/>
+            <span>首页</span>
+          </Link>
+        </li>
+          <li>
+          <Link to='./ShoppingCart'>
+            <i className="iconfont">&#xe68d;</i><br/>
+            <span>分类</span>
+          </Link>
+        </li>
+         <li>
+          <Link to='./ShoppingCart'>
+            <i className="iconfont">&#xe601;</i><br/>
+            <span>约惠</span>
+          </Link>
+        </li>
+         <li>
+          <Link to='./ShoppingCart'>
+            <i className="iconfont">&#xe68c;</i><br/>
+            <span>购物车</span>
+          </Link>
+        </li>
+         <li>
+          <Link to='./ShoppingCart'>
+            <i className="iconfont">&#xe60d;</i><br/>
+            <span>我的</span>
+          </Link>
+        </li>
+        </ul>
+       
          
         </div>
       )
