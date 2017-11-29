f9317ec55b73dd74b603866ea2b083d01464ec57
yujing-0504 <593224885@qq.com>
2017-11-29 10:10:14 +0800

    detailnew

--- project/src/components/addCart.js
+++ project/src/components/addCart.js
@@ -2,14 +2,34 @@ import React,{Component} from 'react';
 import {connect} from  'react-redux';
 import './addCart.css'
 import axios from "axios";
-
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 class AddCart extends Component {
 	
 	render() {
 		var props = this.props
+		var match = this.props.match
 		return(
 			<div className="addcart">
-				添加
+				<div className="detail-bar border-top">
+				    <Link to='./ShoppingCart'>
+				        <span className="icon-dal icon-cart">
+				        	<i className="iconfont">&#xe68c;</i>
+				        </span>
+				        <span className="cart-num">1</span>
+				    </Link>
+				    
+				        
+				        <a className="buy-btn">立即购买</a>
+				        <a className="cart-btn">加入购物车</a>
+				        
+				    
+				</div>
 			</div>
 			
 		)
--- project/src/components/detail.js
+++ project/src/components/detail.js
@@ -2,12 +2,14 @@ import React ,{Component} from 'react'
 import './detail.css'
 import Guess from './guess'
 import Photo from './photo'
+import AddCart from './addCart'
 export default class Detail extends Component{
   render(){
     return(
       <div className="detail">
         <Photo></Photo>
        	<Guess></Guess>
+       	<AddCart></AddCart>
       </div>
       )
   }
